219acc2259f65d71832dc9b1ca221ff7f35aea52
Andrei Bezlepkin <6abez6@gmail.com>
2022-03-17 18:26:56 +0300

    add some elements to MainViewController

--- Quotex/ViewControllers/MainViewController.swift
+++ Quotex/ViewControllers/MainViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class MainViewController: UINavigationController {
     
+    private lazy var cashLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.text = "$ 1,000.00"
+        label.textColor = UIColor(red: 15/255, green: 175/255, blue: 89/255, alpha: 1)
+        return label
+    }()
+    
+    private lazy var choosenLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = UIFont(name: "roboto", size: 22)
+        label.text = "Chose pair"
+        return label
+    }()
+    
+    private lazy var currencyTableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .green
+        
+        return table
+    }()
+     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,9 +45,31 @@ class MainViewController: UINavigationController {
     
     private func prepareUI() {
         
-        self.view.backgroundColor = .red
+        view.backgroundColor = UIColor(red: 29/255, green: 40/255, blue: 51/255, alpha: 1)
         navigationBar.isHidden = true
         self.modalPresentationStyle = .fullScreen
+        
+        
+        view.addSubview(cashLabel)
+        cashLabel.translatesAutoresizingMaskIntoConstraints = false
+        cashLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        cashLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 111).isActive = true
+        cashLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -111).isActive = true
+        cashLabel.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        
+        view.addSubview(choosenLabel)
+        choosenLabel.translatesAutoresizingMaskIntoConstraints = false
+        choosenLabel.topAnchor.constraint(equalTo: cashLabel.bottomAnchor, constant: 27).isActive = true
+        choosenLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 62).isActive = true
+        choosenLabel.centerXAnchor.constraint(equalTo: cashLabel.centerXAnchor).isActive = true
+        choosenLabel.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        
+        view.addSubview(currencyTableView)
+        currencyTableView.translatesAutoresizingMaskIntoConstraints = false
+        currencyTableView.topAnchor.constraint(equalTo: choosenLabel.bottomAnchor, constant: 16).isActive = true
+        currencyTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        currencyTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        
     }
 }
 
